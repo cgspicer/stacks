fc3ce1fbce2601b30b929ef84bcf098e855117a5
cgspicer <cgspicer@gmail.com>
2013-11-22 11:20:03 -0500

    added custom Modernizr and rearranged js

--- htdocs/javascripts/script.js
+++ htdocs/javascripts/script.js
@@ -1,5 +1,12 @@
 $(window).ready(function() {
 
+  /**
+  * Stacks initialization
+  */
+  stacks();
 
+  /*
+  * Write your custom application specific script here
+  */
 
 });
---
+++ htdocs/javascripts/stacks.js
@@ -0,0 +1,13 @@
+//making a global function for dev sake, will wrap in a plugin later
+function stacks() {
+
+  /**
+  * Stacks script
+  */
+
+  //initialize SnapJS
+  var snapper = new Snap({
+    element: $('.stack.page')[0]
+  });
+
+}
